fd4f525ec0b93b35481314f51c563ce5c227f623
unknown <llrocha@NDJ-0031.sds_domain_1.local>
2015-06-30 13:04:05 -0300

    cambios en el profilesController

---
+++ html/app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use Symfony\Component\HttpKernel\Tests\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: llrocha
+ * Date: 30/06/2015
+ * Time: 09:40 AM
+ */
+class ProfilesController extends Controller {
+
+
+
+}
